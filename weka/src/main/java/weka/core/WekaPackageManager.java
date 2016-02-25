08987f69413a3b69a44f2baafa0bee20076189a7
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2016-02-25 04:31:48 +0000

    Added a mechnism to the PluginManager that allows a user-friendly name to be associated with a particular plugin implementation that is declared in a PluginManager.props file. The format for the property file is fully qualified interface/base type=[optional user friendly name]fully qualified implementation class name. Fixed tool tip in AttributeSummarizer. DataVisualizer no longer passes offscreen plot data downstream when restoreData() is called

--- weka/src/main/java/weka/core/WekaPackageManager.java
+++ weka/src/main/java/weka/core/WekaPackageManager.java
@@ -20,19 +20,18 @@
 
 package weka.core;
 
+import weka.core.converters.ConverterUtils;
+import weka.core.logging.Logger;
 import weka.core.packageManagement.DefaultPackageManager;
 import weka.core.packageManagement.Dependency;
 import weka.core.packageManagement.Package;
 import weka.core.packageManagement.PackageConstraint;
 import weka.core.packageManagement.PackageManager;
 import weka.core.packageManagement.VersionPackageConstraint;
-import weka.core.converters.ConverterUtils;
-import weka.core.logging.Logger;
 import weka.gui.GenericObjectEditor;
 import weka.gui.GenericPropertiesCreator;
 import weka.gui.beans.BeansProperties;
 import weka.gui.beans.KnowledgeFlowApp;
-import weka.core.PluginManager;
 import weka.gui.explorer.ExplorerDefaults;
 
 import java.io.BufferedInputStream;
@@ -632,6 +631,7 @@ public class WekaPackageManager {
     try {
       PluginManager.addFromProperties(propsFile);
     } catch (Exception ex) {
+      ex.printStackTrace();
     }
   }
 
