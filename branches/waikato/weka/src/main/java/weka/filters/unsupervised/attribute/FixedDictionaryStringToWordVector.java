ffdd0fba4c521bf4863bad723336442ba2fe2a1a
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2020-05-10 23:30:20 +0000

    Fixed some bugs in DictionaryBuilder and FixedDictionaryStringToWordVector. DictionaryBuilder now writes the total document count into the dictionary file (both text and binary forms) in order to facilitate output of TF-IDF values. Loading a binary dictionary was not setting the output format - fixed. Fixed some option handling in FixedDictionaryStringToWordVectory. For meaningful error messages with existing dictionary files now prints an exception if TF-IDF is requested, but the total document count is not available in the dictionary file

--- branches/waikato/weka/src/main/java/weka/filters/unsupervised/attribute/FixedDictionaryStringToWordVector.java
+++ branches/waikato/weka/src/main/java/weka/filters/unsupervised/attribute/FixedDictionaryStringToWordVector.java
@@ -136,7 +136,7 @@ import weka.gui.FilePropertyMetadata;
  * <!-- options-end -->
  *
  * @author Mark Hall (mhall{[at]}pentaho{[dot]}com)
- * @version $Revision: 14508 $
+ * @version $Revision: 15573 $
  */
 public class FixedDictionaryStringToWordVector extends SimpleStreamFilter
   implements UnsupervisedFilter, EnvironmentHandler, WeightedInstancesHandler {
@@ -379,7 +379,7 @@ public class FixedDictionaryStringToWordVector extends SimpleStreamFilter
     description = "Set whether the word frequencies should be transformed into\n"
       + "log(1+fij), where fij is the frequency of word i in document (instance) "
       + "j.",
-    commandLineParamName = "T", commandLineParamSynopsis = "-T",
+    commandLineParamName = "T", commandLineParamSynopsis = "-T", commandLineParamIsFlag = true,
     displayOrder = 7)
   public void setTFTransform(boolean TFTransform) {
     m_vectorizer.setTFTransform(TFTransform);
@@ -409,7 +409,7 @@ public class FixedDictionaryStringToWordVector extends SimpleStreamFilter
     description = "Set whether the word frequencies in a document should be "
       + "transformed into\nfij*log(num of Docs/num of docs with word i), "
       + "where fij is the frequency\nof word i in document (instance) j.",
-    commandLineParamName = "I", commandLineParamSynopsis = "-I",
+    commandLineParamName = "I", commandLineParamSynopsis = "-I", commandLineParamIsFlag = true,
     displayOrder = 8)
   public void setIDFTransform(boolean IDFTransform) {
     m_vectorizer.setIDFTransform(IDFTransform);
